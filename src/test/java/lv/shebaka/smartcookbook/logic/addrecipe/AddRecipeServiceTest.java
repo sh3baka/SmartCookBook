6a2d4a91adbc9fcbcfe881203736d8a772ad76d5
sh3baka <napsterlv@gmail.com>
2018-03-13 05:40:48 +0200

    Tests

---
+++ src/test/java/lv/shebaka/smartcookbook/logic/addrecipe/AddRecipeServiceTest.java
@@ -0,0 +1,49 @@
+package lv.shebaka.smartcookbook.logic.addrecipe;
+
+import lv.shebaka.smartcookbook.data.RecipeDatabase;
+import lv.shebaka.smartcookbook.logic.Error;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddRecipeServiceTest {
+
+    private RecipeDatabase database;
+    private AddRecipeValidator validator;
+    private AddRecipeService service;
+
+    @Before
+    public void init(){
+        database = Mockito.mock(RecipeDatabase.class);
+        validator = Mockito.mock(AddRecipeValidator.class);
+        service = new AddRecipeService(database, validator);
+    }
+
+    @Test
+    public void shouldReturnSuccess(){
+        List<Error> errors = new ArrayList<>();
+        Mockito.when(validator.validate("title","desc")).thenReturn(errors);
+
+        AddRecipeResponse response = service.addRecipe("title","desc");
+
+        assertEquals(response.isSuccess(), true);
+        assertEquals(response.getErrors(), null);
+    }
+
+    @Test
+    public void shouldReturnFalse(){
+        List<Error> errors = new ArrayList<>();
+        errors.add(new Error("title", "errorMsg"));
+        Mockito.when(validator.validate(null, "desc")).thenReturn(errors);
+
+        AddRecipeResponse response = service.addRecipe(null, "desc");
+
+        assertEquals(response.isSuccess(), false);
+        assertEquals(response.getErrors(), errors);
+    }
+}
---
+++ src/test/java/lv/shebaka/smartcookbook/logic/addrecipe/AddRecipeValidatorTest.java
@@ -0,0 +1,38 @@
+package lv.shebaka.smartcookbook.logic.addrecipe;
+
+import lv.shebaka.smartcookbook.data.RecipeDatabase;
+import lv.shebaka.smartcookbook.logic.Error;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class AddRecipeValidatorTest {
+
+    private RecipeDatabase database;
+    private AddRecipeValidator validator;
+
+    @Before
+    public void init(){
+        database = Mockito.mock(RecipeDatabase.class);
+        validator = new AddRecipeValidator(database);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenTitleIsNull(){
+        List<Error> errors = validator.validate(null,"description");
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).getField(), "title");
+        assertEquals(errors.get(0).getErrorMsg(),"Must not be empty");
+    }
+
+    @Test
+    public void shouldReturnListEmpty(){
+        List<Error> errors = validator.validate("title", "desc");
+        assertEquals(errors.size(), 0);
+    }
+
+}
