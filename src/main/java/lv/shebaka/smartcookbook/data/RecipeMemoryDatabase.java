f2e290c25d140363840323fe74079984275dd798
sh3baka <napsterlv@gmail.com>
2018-04-29 06:37:12 +0300

    OldDB comment

--- src/main/java/lv/shebaka/smartcookbook/data/RecipeMemoryDatabase.java
+++ src/main/java/lv/shebaka/smartcookbook/data/RecipeMemoryDatabase.java
@@ -1,40 +1,40 @@
-package lv.shebaka.smartcookbook.data;
-
-import lv.shebaka.smartcookbook.domain.Recipe;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-public class RecipeMemoryDatabase implements RecipeDatabase {
-
-
-    private List<Recipe> recipes = new ArrayList<>();
-
-    @Override
-
-    public void add(Recipe recipe) {
-        recipes.add(recipe);
-    }
-
-    @Override
-
-    public void remove(Recipe recipe) {
-        recipes.remove(recipe);
-    }
-
-    @Override
-
-    public List<Recipe> getAllRecipes() {
-        List<Recipe> allRecipes = new ArrayList<>();
-        allRecipes.addAll(recipes);
-        return allRecipes;
-    }
-
-    @Override
-
-    public Optional<Recipe> findByTitle(final String title) {
-        return recipes.stream()
-                .filter(p -> p.getTitle().equals(title)).findFirst();
-    }
-}
+//package lv.shebaka.smartcookbook.data;
+//
+//import lv.shebaka.smartcookbook.domain.Recipe;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//public class RecipeMemoryDatabase implements RecipeDatabase {
+//
+//
+//    private List<Recipe> recipes = new ArrayList<>();
+//
+//    @Override
+//
+//    public void add(Recipe recipe) {
+//        recipes.add(recipe);
+//    }
+//
+//    @Override
+//
+//    public void remove(Recipe recipe) {
+//        recipes.remove(recipe);
+//    }
+//
+//    @Override
+//
+//    public List<Recipe> getAllRecipes() {
+//        List<Recipe> allRecipes = new ArrayList<>();
+//        allRecipes.addAll(recipes);
+//        return allRecipes;
+//    }
+//
+//    @Override
+//
+//    public Optional<Recipe> findByTitle(final String title) {
+//        return recipes.stream()
+//                .filter(p -> p.getTitle().equals(title)).findFirst();
+//    }
+//}
